4b2dbfa72fbaa91e909ce609890b9577f28fb981
jannik <jannik.damerau@gmail.com>
2023-10-30 16:43:06 +0100

    initial commit

---
+++ domainLogic/src/verkaufsAutomat.java
@@ -0,0 +1,16 @@
+public class verkaufsAutomat {
+    public boolean create(ObstkuchenImp input){
+        return false;
+    }
+
+    public boolean delete(int fachnummer){
+        return false;
+    }
+
+    public boolean update(int fachnummer){
+        return false;
+    }
+    public ObstkuchenImp read(int fachnummer){
+        return lager[fachnummer];
+    }
+}
